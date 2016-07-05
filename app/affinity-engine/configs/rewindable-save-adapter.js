49ab7086c5f0adb88853dc11763ac7317ed2bf78
null null null <null@revolution.cheap>
2016-07-04 20:21:22 -0700

    further path improvements

--- app/affinity-engine/configs/rewindable-save-adapter.js
+++ app/affinity-engine/configs/rewindable-save-adapter.js
@@ -1,1 +1,1 @@
-export { default } from 'affinity-engine-stage/affinity-engine/configs/rewindable-save-adapter';
+export { default } from 'affinity-engine-rewindable-save-adapter/affinity-engine/configs/rewindable-save-adapter';
