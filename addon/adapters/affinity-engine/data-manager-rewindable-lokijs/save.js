97dabffa87e680515640a73218e870c0b447dfc2
null null null <null@affinitygamescollective.org>
2017-11-01 11:16:43 -0400

    use a single adapter instance

--- addon/adapters/affinity-engine/data-manager-rewindable-lokijs/save.js
+++ addon/adapters/affinity-engine/data-manager-rewindable-lokijs/save.js
@@ -1,8 +1,9 @@
 import { LokiJSAdapter } from 'ember-lokijs';
+import adapter from './adapter';
 
 export default LokiJSAdapter.extend({
   indices: ['dataGroup', 'isAutosave'],
   lokiOptions: {
-    adapter: new LokiIndexedAdapter()
+    adapter
   }
 });
